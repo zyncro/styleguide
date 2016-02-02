2b16c336c0f5b6890cb6cebf16e3805fac9231db
Pedro Aspiazu <pedro.aspiazu@zyncro.com>
2016-02-02 13:36:14 +0100

    message

--- app/main/styleguide/styleguide.js
+++ app/main/styleguide/styleguide.js
@@ -202,20 +202,22 @@ angular.module('myApp.styleguide', ['ngRoute','ngSanitize', 'ui.select'])
     /**
     * Carousel
     */
-
-    $scope.myInterval = 5000;
-    var slides = $scope.slides = [];
-    $scope.addSlide = function() {
+  $scope.myInterval = 5000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
     var newWidth = 600 + slides.length + 1;
     slides.push({
-    image: 'http://placekitten.com/' + newWidth + '/300',
-    text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-    ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+      image: 'http://placekitten.com/' + newWidth + '/300',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
     });
-    };
-    for (var i=0; i<4; i++) {
+  };
+  for (var i=0; i<4; i++) {
     $scope.addSlide();
-    }
+  }
+
+
+
 
 
     /**
